065a8523fd0d1ec6356a4e87b51fc4e1eddfa76d
Jan <janhajk@gmail.com>
2013-11-20 06:46:38 -0500

    added updateIntervals variables to config file

--- sample.config.js
+++ sample.config.js
@@ -36,6 +36,9 @@ exports.feeds    = {
              }
 };
 
+exports.updateIntervalFeeds = 1000*60*20;
+exports.updateIntervalInfos = 10000;
+
 exports.urlRegex = new RegExp("^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?");
 exports.emailRegex = new RegExp("^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$");
 
