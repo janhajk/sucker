aef6f6d56743fed31add82fcf96a9ee16159408b
janhajk <janhajk@gmail.com>
2014-11-06 12:53:17 +0000

    log

--- database/database.js
+++ database/database.js
@@ -1,15 +1,16 @@
 var config = require(__dirname + '/../config.js');
 var mongoose = require('mongoose');
+var utils    = require(__dirname + '../lib/utils.js');
 
 
 exports.connect = function(callback) {
-        console.log("Connecting to database...");
+        utils.log('Connecting to database...');
         mongoose.connect(config.database);
 
         var db = mongoose.connection;
         db.on('error', console.error.bind(console, 'Connection error:'));
         db.once('open', function() {
-                console.log("Connected !");
+                utils.log('Connected to DB successfully!');
                 callback();
         });
 };
