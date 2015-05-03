8169b6a18a179a07a4e4abe93abecfc3edb60008
janhajk <janhajk@gmail.com>
2015-05-03 14:29:33 +0000

    implement bootstrap

--- public/javascript/sucker.js
+++ public/javascript/sucker.js
@@ -174,6 +174,7 @@
             var img = document.createElement('img');
             img.src = imageUrl;
             img.className = 'thumbPosterImg thumb thumbnail';
+            img.style.height = '100px';
             var divTitle = document.createElement('div');
             var div = document.createElement('div');
             div.className = 'thumbPoster';
